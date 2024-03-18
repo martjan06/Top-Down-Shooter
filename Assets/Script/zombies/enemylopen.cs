d3f245ff8fbb7aa750eb4ddd700489fe49f3b184
martjan06 <mart-jan@bmwcs.nl>
2024-03-18 13:50:02 +0100

    waypoints

--- Assets/Script/zombies/enemylopen.cs
+++ Assets/Script/zombies/enemylopen.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class enemylopen : MonoBehaviour
 {
-    private GameObject pointA;
-    private GameObject pointB;
-    // Start is called before the first frame update
+
+    [SerializeField]
+    Transform[] waypoints;
+
+    [SerializeField]
+    float moveSpeed = 2f;
+
+    int waypointIndex = 0;
+
     void Start()
     {
-        
+        transform.position = waypoints[waypointIndex].transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,
+                                                waypoints[waypointIndex].transform.position,
+                                                moveSpeed * Time.deltaTime);
+
+        if (transform.position == waypoints[waypointIndex].transform.position)
+        {
+            waypointIndex += 1;
+        }
+
+        if (waypointIndex == waypoints.Length)
+            waypointIndex = 0;
     }
 }
