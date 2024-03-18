22e668e74eae08c2686e9a570a3761be6a214757
martjan06 <mart-jan@bmwcs.nl>
2024-03-18 10:05:16 +0100

    v1.9

--- Assets/Script/shooting.cs
+++ Assets/Script/shooting.cs
@@ -8,7 +8,7 @@ public class shooting : MonoBehaviour
     public Transform Shots;
     public GameObject Player;
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
