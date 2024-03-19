a362fac9a896ca07ea11ad92492ddf333313393c
martjan06 <mart-jan@bmwcs.nl>
2024-03-19 09:55:30 +0100

    v3.1

--- Assets/Script/Player/health player.cs
+++ Assets/Script/Player/health player.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class healthplayer : MonoBehaviour
 {
     public int health = 10;
     public int playerhealth;
+    public string dead;
     void Start()
     {
         playerhealth = health;
@@ -17,7 +20,7 @@ public class healthplayer : MonoBehaviour
         if (playerhealth <= 0)
         {
             Destroy(gameObject);
-
+            SceneManager.LoadScene(dead);
         }
     }
 }
